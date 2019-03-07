cd4122ee33b5ec0bc9c50f6f6c302fedaac30410
Aravind Hebbali <hebbali.aravind@gmail.com>
2019-03-07 10:00:04 +0000

    version 0.1.1

--- R/vdist-app.R
+++ R/vdist-app.R
@@ -1,13 +1,17 @@
-#' @importFrom shiny runApp
-#' @title Launch Shiny App
-#' @description Launches shiny app
+#' @title Launch shiny app
+#' @description Launches shiny app for visualizing distributions.
 #' @examples
 #' \dontrun{
-#' vdist_launch_app()
+#' vdist_launch_app ()
 #' }
 #' @export
 #'
-vdist_launch_app <- function() {
-  shiny::runApp(appDir = system.file("application", package = "vistributions"))
-}
+vdist_launch_app  <- function() {
 
+	check_suggests('shinyBS')
+	check_suggests('shinycssloaders')
+	check_suggests('shinythemes')
+
+	shiny::runApp(appDir = system.file("application", package = "vistributions"))
+}
+ 
---
+++ R/zzz.R
@@ -0,0 +1,37 @@
+#' @importFrom magrittr %>%
+.onAttach <- function(...) {
+
+  if (!interactive() || stats::runif(1) > 0.1) return()
+
+  pkgs <- utils::available.packages()
+  
+  cran_version <- 
+    pkgs %>%
+    magrittr::extract("vistributions", "Version") %>%
+    package_version()
+
+  local_version <- utils::packageVersion("vistributions")
+  behind_cran <- cran_version > local_version
+
+  tips <- c(
+    "Learn more about vistributions at https://github.com/rsquaredacademy/vistributions/.",
+    "Use suppressPackageStartupMessages() to eliminate package startup messages.",
+    "Need help getting started with regression models? Visit: https://www.rsquaredacademy.com",
+    "Check out our interactive app for quick data exploration. Visit: https://apps.rsquaredacademy.com/."
+  )
+
+  tip <- sample(tips, 1)
+
+  if (interactive()) {
+    if (behind_cran) {
+      msg <- "A new version of vistributions is available with bug fixes and new features."
+      packageStartupMessage(msg, "\nWould you like to install it?")
+      if (utils::menu(c("Yes", "No")) == 1) {
+        utils::update.packages("vistributions")
+      } 
+    } else {
+      packageStartupMessage(paste(strwrap(tip), collapse = "\n"))
+    }   
+  }   
+
+}
